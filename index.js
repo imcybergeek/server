043a6ee8f8b512001a5db02f21a79b73fb28c0ac
Jatin Joshi <imcybergeek@gmail.com>
2022-08-18 18:41:28 +0530

    github webook integration

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
-const http = require('http');
+const express = require('express')
+const path = require('path')
 
-// Create server object
-http.createServer((req, res) => {
-// Write Response
-res.write('Hello World');
-res.end()
-}).listen(5000, () => console.log('Server running...'));
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+app.post('/hook', (req, res) => {
+    console.log(req.body, req.headers)
+    res.status(200)
+})
+
+const secret = "afgsfjblbkhafgsdmjgmfdhghdhfd"
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
