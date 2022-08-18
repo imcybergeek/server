7438d2f8075a095ace814e42a64aa167eccb88bf
Jatin Joshi <imcybergeek@gmail.com>
2022-08-19 01:58:06 +0530

    second webhook attempt

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
 app.get('/', (req, res) => {
-    res.send("♥️")
+    res.send("👍")
 })
 
 const PORT = process.env.PORT || 5000
