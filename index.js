4098d984a9175abdf095e17f7e1c2744f4d7250d
Jatin Joshi <imcybergeek@gmail.com>
2022-08-19 02:46:15 +0530

    finally done

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ app.get('/', (req, res) => {
     res.send('👌')
 })
 
-const PORT = process.env.PORT || 6000
+const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
