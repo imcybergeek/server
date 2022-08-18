f777f04ce95e117325eba5bcc4556e2f84eea2ab
Jatin Joshi <imcybergeek@gmail.com>
2022-08-19 02:56:16 +0530

    done

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
 app.get('/', (req, res) => {
-    res.send('😂👌')
+    res.send('👌')
 })
 
 const PORT = process.env.PORT || 5000
