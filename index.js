1708835958bf2ac00f849f4067ea7f3439670c58
Jatin Joshi <imcybergeek@gmail.com>
2022-08-22 10:40:14 +0530

    emoji

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
 app.get('/', (req, res) => {
-    res.send('Gurgaon')
+    res.send('👌')
 })
 
 const PORT = process.env.PORT || 5000
